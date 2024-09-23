ceacc1cc2006ada476d23131d6db3003f7567aaa
Gusarov Faddey <gusarovfg@gmail.com>
2024-09-23 17:22:31 +0400

    feat: Добавлен метод для получения курсов из локального репозитория

--- lib/repository/local_repository.dart/local_courses_repository.dart
+++ lib/repository/local_repository.dart/local_courses_repository.dart
@@ -1,10 +1,23 @@
+import 'package:test_courses/hive_service/hive_service.dart';
+import 'package:test_courses/models/courses_model/courses_model.dart';
+
 class LocalCoursesRepository {
   static final LocalCoursesRepository _singleton =
       LocalCoursesRepository._internal();
+
+  final hive = HiveService();
 
   factory LocalCoursesRepository() {
     return _singleton;
   }
 
   LocalCoursesRepository._internal();
+
+  Courses getCourses() {
+    Courses courses = Courses(courses: []);
+    hive.getCourses().then((value) {
+      courses = value.first;
+    });
+    return courses;
+  }
 }
